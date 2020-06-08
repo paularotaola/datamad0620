97e47ba7c63167a70fb337209bfdeea14c8edc7b
Paula Rodriguez <paularotaola@gmail.com>
2020-06-08 20:37:21 +0200

    Con correciones

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -4,12 +4,12 @@ import random
 # Soldier
 class Soldier:
 
-    def __init__ (self, health, strenght):
+    def __init__ (self, health, strength):
         self.health = health
-        self.strenght = strenght
+        self.strength = strength
     
     def attack(self):
-        return self.strenght 
+        return self.strength 
 
     def receiveDamage(self, damage):
         self.health = self.health- damage
@@ -18,8 +18,8 @@ class Soldier:
 # Viking
 class Viking (Soldier):
 
-    def __init__(self, name, health, strenght):
-        super().__init__(self, health,strenght)
+    def __init__(self,name, health, strength):
+        super().__init__(health,strength)
         self.name = name
 
     def receiveDamage(self, damage):
@@ -36,33 +36,33 @@ class Saxon(Soldier):
     
     def receiveDamage(self, damage):
         self.health=self.health-damage
-        if self.health < 0:
+        if self.health > 0:
             return f"A Saxon has received {damage} points of damage"
         else:
-            return f"A Saxon has died in act of combat"
+            return f"A Saxon has died in combat"
 # War
 class War:
     def __init__(self):
         self.vikingArmy = []
         self.saxonArmy = []
 
-    def addViking(self):
-        self.vikingArmy.append("1")
-    def addSaxon(self, Saxon):
-        self.saxonArmy.append("1")
+    def addViking(self, viking):
+        self.vikingArmy.append("viking")
+    def addSaxon(self, saxon):
+        self.saxonArmy.append("saxon")
 
     def vikingAttack(self):
         viking_rd =random.choice(self.vikingArmy)
         saxon_rd =random.choice(self.saxonArmy)
-        dmg = saxon_rd.receiveDamage(viking_rd.strenght)
+        dmg = saxon_rd.receiveDamage(viking_rd.strength)
         if saxon_rd.health <= 0:
             self.saxonArmy.remove(saxon_rd)
         return dmg
 
-    def saxonAttack():
+    def saxonAttack(self):
         viking_rd =random.choice(self.vikingArmy)
         saxon_rd =random.choice(self.saxonArmy)
-        dmg = viking_rd.receiveDamage(saxon_rd.strenght)
+        dmg = viking_rd.receiveDamage(saxon_rd.strength)
         if viking_rd.health <= 0:
             self.vikingArmy.remove(viking_rd)
         return dmg
@@ -73,4 +73,4 @@ class War:
         elif len(self.vikingArmy) ==0:
             return "Saxons have fought for their lives and survive another day..."
         elif (len(self.saxonArmy) > 0) or (len(self.vikingArmy) > 0):
-            return "Vikings and Saxons are still in the thick of battle"
+            return "Vikings and Saxons are still in the thick of battle."
